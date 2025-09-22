fcdbc994890fb163c5a0815e621bca44343ba9ae
João Victor <144742430+jvictormo@users.noreply.github.com>
2025-09-22 17:05:55 -0300

    Adjusting FastAPI initializing in main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,14 @@ from routes.area_imovel_projeto import area_imovel_router
 from routes.auth import auth
 from core.database import ensure_indexes
 
-#Declara uma constante para o FastAPI
-app = FastAPI()
-
 #Cria índices no banco de dados ao iniciar a aplicação (evita usuários duplicados)
 async def lifespan(app: FastAPI):
     await ensure_indexes()
     yield
 
+#Declara uma constante para o FastAPI
+app = FastAPI(lifespan=lifespan)
+
 #Inclui as rotas no backend
 app.include_router(area_imovel_router)
 app.include_router(auth)
-
-
-
